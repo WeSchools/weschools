599c692f646599ba678c56a4bcd7c2fadc8fcf55
Felix <yangfanustc@gmail.com>
2015-08-16 14:03:42 +0000

    add index page demo

---
+++ index.php
@@ -0,0 +1,101 @@
+<html>
+	<head>
+			<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<style>
+.nav li:hover ul {
+display: block;
+}
+
+.sidebar-left{
+float:left; 
+width:15%;
+border-right:1px #ddd solid;
+}
+
+.sidebar-left ul{
+																																											list-style-type: none;
+																																																														}
+																																																																	
+																																																																				.sidebar-left ul li{
+																																																																									height:45px;
+																																																																												}
+																																																																															
+																																																																																	</style>
+																																																																																		</head>
+																																																																																			<body>
+																																																																																					<nav class="navbar navbar-inverse">
+																																																																																					  <div class="container-fluid">
+																																																																																					      <div class="navbar-header">
+																																																																																						        <a class="navbar-brand" href="#">微校</a>
+																																																																																								    </div>
+
+																																																																																									    <!-- Collect the nav links, forms, and other content for toggling -->
+																																																																																										    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+																																																																																											      <ul class="nav navbar-nav">
+																																																																																												          <li class="active"><a href="#">课程 <span class="sr-only">(current)</span></a></li>
+																																																																																														          <li><a href="#">小组</a></li>
+																																																																																																          <li class="dropdown">
+																																																																																																		            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">资源 <span class="caret"></span></a>
+																																																																																																					          <ul class="dropdown-menu">
+																																																																																																							              <li><a href="#">Action</a></li>
+																																																																																																										              <li role="separator" class="divider"></li>
+																																																																																																													              <li><a href="#"> link</a></li>
+																																																																																																																            </ul>
+																																																																																																																			        </li>
+																																																																																																																					      </ul>
+
+																																																																																																																						        <ul class="nav navbar-nav navbar-right">
+																																																																																																																								        <li><a href="#">我的消息</a></li>
+																																																																																																																										        <li class="dropdown">
+																																																																																																																												          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Felix <span class="caret"></span></a>
+																																																																																																																														            <ul class="dropdown-menu">
+																																																																																																																																	            <li><a href="#">Action</a></li>
+																																																																																																																																				            <li><a href="#">Another action</a></li>
+																																																																																																																																							            <li role="separator" class="divider"></li>
+																																																																																																																																										            <li><a href="#">link</a></li>
+																																																																																																																																													          </ul>
+																																																																																																																																															          </li>
+																																																																																																																																																	        </ul>
+																																																																																																																																																			    </div><!-- /.navbar-collapse -->
+																																																																																																																																																				  </div><!-- /.container-fluid -->
+																																																																																																																																																				  </nav>
+
+
+																																																																																																																																																				  	<div class="sidebar-left">
+																																																																																																																																																							<ul>
+																																																																																																																																																										<li><a href="#">最近活动</a></li>
+																																																																																																																																																													<li><a href="#">我的日历</a></li>
+																																																																																																																																																																<li><a href="#">我的消息</a></li>
+																																																																																																																																																																			<li><a href="#">选择学校</a></li>
+																																																																																																																																																																						<li><a href="#">邀请同事</a></li>
+																																																																																																																																																																								</ul>
+																																																																																																																																																																									</div>
+																																																																																																																																																																										
+																																																																																																																																																																											<div style="width:60%;">
+																																																																																																																																																																												</div>
+																																																																																																																																																																													
+																																																																																																																																																																														<div>
+																																																																																																																																																																															</div>
+
+
+
+
+
+
+																																																																																																																																																																															<nav class="navbar navbar-default navbar-fixed-bottom">
+																																																																																																																																																																															  <div class="container">
+																																																																																																																																																																															      <div class="navbar-header">
+																																																																																																																																																																																        <a class="navbar-brand" href="#">关于我们</a>
+																																																																																																																																																																																		    </div>
+																																																																																																																																																																																				   <div class="navbar-header">
+																																																																																																																																																																																				         <a class="navbar-brand" href="#">帮助</a>
+																																																																																																																																																																																						     </div>
+																																																																																																																																																																																							   </div>
+																																																																																																																																																																																							   </nav>
+																																																																																																																																																																																							   	</body>
+																																																																																																																																																																																								</html>
